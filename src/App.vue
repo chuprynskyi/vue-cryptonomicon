8bf5552d64a170387006ec9d0f2262a5a3e9aa26
Aleksandr Chuprynskyi <chuprynskyi_ak16@nuwm.edu.ua>
2022-04-27 21:53:55 +0300

    Add buttons the last and next page

--- src/App.vue
+++ src/App.vue
@@ -67,9 +67,26 @@
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
+          >
+            Назад</button
+          ><button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="hasNextPage"
+            @click="page = page + 1"
+          >
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter" /></div>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filtredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -162,10 +179,25 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      page: 1,
+      filter: '',
+      hasNextPage: true,
     };
   },
 
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries(),
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem('cryptonomicon-list');
 
     if (tickersData) {
@@ -177,6 +209,19 @@ export default {
   },
 
   methods: {
+    filtredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filtredTickers = this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter),
+      );
+
+      this.hasNextPage = filtredTickers.length > end;
+
+      return filtredTickers.slice(start, end);
+    },
+
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -201,6 +246,7 @@ export default {
       };
 
       this.tickers.push(currentTicker);
+      this.filter = '';
 
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
       this.subscribeToUpdates(currentTicker.name);
@@ -220,6 +266,24 @@ export default {
       const minValue = Math.min(...this.graph);
       return this.graph.map(
         (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue),
+      );
+    },
+  },
+
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&&page=${this.page}`,
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&&page=${this.page}`,
       );
     },
   },
